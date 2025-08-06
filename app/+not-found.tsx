aee318af8bcfe7d2d51ff5ac54fd8a4dace0167f
Scrumlord123 <mikkel@cargo-bi.com>
2025-08-06 16:39:01 +0200

    Flyttet frontend filer

--- app/+not-found.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function NotFoundScreen() {
-  return (
-    <View style={{ padding: 20 }}>
-      <Text>404 - Siden blev ikke fundet</Text>
-    </View>
-  );
-}
